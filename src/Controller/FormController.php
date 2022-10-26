01127b9ec245458f7dc6aececc17f0f8384d3e67
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-10-26 19:01:03 +0200

    added verification

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -4,11 +4,70 @@ namespace App\Controller;
 
 class FormController extends AbstractController
 {
-    /**
-     * Display home page
-     */
-    public function form(): string
+    public const MAX_FULLNAME_LENGTH = 90;
+    public const MAX_EMAIL_LENGTH = 255;
+    public const SUBJECTS = [
+        'info' => 'Demande d\'informations',
+        'remove' => 'Annuler une inscription à une sortie',
+        'register' => 'Demande d\'adhésion',
+    ];
+
+    public function verification(): string
     {
-        return $this->twig->render('Form/form.html.twig');
+        $contact = [];
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $contact = array_map('trim', $_POST);
+
+            $this->verifEmpty($errors, $contact);
+
+            if (strlen($contact['fullName']) > self::MAX_FULLNAME_LENGTH) {
+                $errors[] = 'Le nom doit être inférieur à ' . self::MAX_FULLNAME_LENGTH . ' caractères';
+            }
+
+            if (strlen($contact['email']) > self::MAX_EMAIL_LENGTH) {
+                $errors[] = 'L\'email doit être inférieur à ' . self::MAX_EMAIL_LENGTH . ' caractères';
+            }
+
+            if (!filter_var(($contact['email']), FILTER_VALIDATE_EMAIL)) {
+                $errors[] = 'Le format d\'email est incorrect';
+            }
+
+            if (!key_exists($contact['subject'], self::SUBJECTS)) {
+                $errors[] = 'Le sujet est incorrect.';
+            }
+
+            if (empty($errors)) {
+                //Do somethong, for example : send email, put informations in database
+                // and redirection
+                header('location: /contact?message=success');
+                return '';
+            }
+        }
+        return $this->twig->render(
+            'Form/form.html.twig',
+            ['errors' => $errors, 'contact' => $contact, 'subjects' => self::SUBJECTS]
+        );
+    }
+
+    public function verifEmpty(array $errors, array $contact): array
+    {
+        if (empty($contact['fullName'])) {
+            $errors[] = 'Le nom est obligatoire';
+        }
+
+        if (empty($contact['email'])) {
+            $errors[] = 'L\'email est obligatoire';
+        }
+
+        if (empty($contact['subject'])) {
+            $errors[] = 'Le sujet est obligatoire';
+        }
+
+        if (empty($contact['message'])) {
+            $errors[] = 'Le message est obligatoire';
+        }
+
+        return $errors;
     }
 }
