fccb44a6982ec6d01ae9a62022fcc662fdb85a1f
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-10-27 13:40:25 +0200

    added verification method in private

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -12,30 +12,15 @@ class FormController extends AbstractController
         'register' => 'Demande d\'adhésion',
     ];
 
-    public function verification(): string
+    public function index(string $message = ''): string
     {
         $contact = [];
         $errors = [];
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $contact = array_map('trim', $_POST);
 
-            $this->verifEmpty($errors, $contact);
-
-            if (strlen($contact['fullName']) > self::MAX_FULLNAME_LENGTH) {
-                $errors[] = 'Le nom doit être inférieur à ' . self::MAX_FULLNAME_LENGTH . ' caractères';
-            }
-
-            if (strlen($contact['email']) > self::MAX_EMAIL_LENGTH) {
-                $errors[] = 'L\'email doit être inférieur à ' . self::MAX_EMAIL_LENGTH . ' caractères';
-            }
-
-            if (!filter_var(($contact['email']), FILTER_VALIDATE_EMAIL)) {
-                $errors[] = 'Le format d\'email est incorrect';
-            }
-
-            if (!key_exists($contact['subject'], self::SUBJECTS)) {
-                $errors[] = 'Le sujet est incorrect.';
-            }
+            $errors = $this->verification($contact);
 
             if (empty($errors)) {
                 //Do somethong, for example : send email, put informations in database
@@ -46,12 +31,28 @@ class FormController extends AbstractController
         }
         return $this->twig->render(
             'Form/form.html.twig',
-            ['errors' => $errors, 'contact' => $contact, 'subjects' => self::SUBJECTS]
+            ['errors' => $errors, 'contact' => $contact, 'subjects' => self::SUBJECTS, 'message' => $message]
         );
     }
-
-    public function verifEmpty(array $errors, array $contact): array
+    private function verification(array $contact): array
     {
+        $errors = [];
+
+        if (strlen($contact['fullName']) > self::MAX_FULLNAME_LENGTH) {
+            $errors[] = 'Le nom doit être inférieur à ' . self::MAX_FULLNAME_LENGTH . ' caractères';
+        }
+
+        if (strlen($contact['email']) > self::MAX_EMAIL_LENGTH) {
+            $errors[] = 'L\'email doit être inférieur à ' . self::MAX_EMAIL_LENGTH . ' caractères';
+        }
+
+        if (!filter_var(($contact['email']), FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'Le format d\'email est incorrect';
+        }
+
+        if (!key_exists($contact['subject'], self::SUBJECTS)) {
+            $errors[] = 'Le sujet est incorrect.';
+        }
         if (empty($contact['fullName'])) {
             $errors[] = 'Le nom est obligatoire';
         }
@@ -62,10 +63,6 @@ class FormController extends AbstractController
 
         if (empty($contact['subject'])) {
             $errors[] = 'Le sujet est obligatoire';
-        }
-
-        if (empty($contact['message'])) {
-            $errors[] = 'Le message est obligatoire';
         }
 
         return $errors;
