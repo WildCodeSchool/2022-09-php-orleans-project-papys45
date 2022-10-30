2829d802335ed790b9af299524d6cd7fc2709362
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-10-30 18:00:30 +0100

    added skeleton of mvc

--- src/Controller/AdminMembersController.php
+++ src/Controller/AdminMembersController.php
@@ -13,4 +13,23 @@ class AdminMembersController extends AbstractController
 
         return $this->twig->render('Admin/members.html.twig', ['members' => $members]);
     }
+
+    public function add(): ?string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $member = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, insert and redirection
+            $adminMembersManager = new AdminMembersManager();
+            $adminMembersManager->insert($member);
+
+            header('Location:/admin/membres');
+            return null;
+        }
+
+        return $this->twig->render('Item/add.html.twig');
+    }
 }
--- src/Model/AdminMembersManager.php
+++ src/Model/AdminMembersManager.php
@@ -7,4 +7,14 @@ use PDO;
 class AdminMembersManager extends AbstractManager
 {
     public const TABLE = 'member';
+
+
+    public function insert(array $item): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`title`) VALUES (:title)");
+        $statement->bindValue('title', $item['title'], PDO::PARAM_STR);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
 }
