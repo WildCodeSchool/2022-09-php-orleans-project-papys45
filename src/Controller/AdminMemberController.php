7020056121be74ece608e78a04382d75d0dc03ab
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-11-02 21:00:00 +0100

    added request to unlick idPhoto

--- src/Controller/AdminMemberController.php
+++ src/Controller/AdminMemberController.php
@@ -2,13 +2,13 @@
 
 namespace App\Controller;
 
-use App\Model\AdminMembersManager;
+use App\Model\AdminMemberManager;
 
-class AdminMembersController extends AbstractController
+class AdminMemberController extends AbstractController
 {
     public function index(): string
     {
-        $adminMembersManager = new AdminMembersManager();
+        $adminMembersManager = new AdminMemberManager();
         $members = $adminMembersManager->selectAll();
 
         return $this->twig->render('Admin/members.html.twig', ['members' => $members]);
@@ -18,8 +18,14 @@ class AdminMembersController extends AbstractController
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $id = trim($_POST['id']);
-            $adminMembersManager = new AdminMembersManager();
+            $adminMembersManager = new AdminMemberManager();
+            $idPhoto = $adminMembersManager->idPhoto((int) $id);
             $adminMembersManager->delete((int)$id);
+
+            if (file_exists('upload/' . $idPhoto[0])) {
+                unlink('upload/' . $idPhoto[0]);
+                echo "Le fichier a été supprimé";
+            }
 
             header('Location:/admin/membres');
         }
