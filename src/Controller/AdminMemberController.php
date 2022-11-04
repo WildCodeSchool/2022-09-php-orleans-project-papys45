d2f067012dae6f427a5d576caa858234c6aefcfe
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-11-04 16:09:26 +0100

    correction PR

--- src/Controller/AdminMemberController.php
+++ src/Controller/AdminMemberController.php
@@ -6,6 +6,8 @@ use App\Model\MemberManager;
 
 class AdminMemberController extends AbstractController
 {
+    public const UPLOAD_DIR = 'upload/';
+
     public function index(): string
     {
         $adminMembersManager = new MemberManager();
@@ -22,8 +24,8 @@ class AdminMemberController extends AbstractController
             $idPhoto = $adminMembersManager->idPhoto(intval($id));
             $adminMembersManager->delete(intval($id));
 
-            if (!empty($idPhoto['photo']) && file_exists('upload/' . $idPhoto['photo'])) {
-                unlink('upload/' . $idPhoto['photo']);
+            if (!empty($idPhoto['photo']) && file_exists(self::UPLOAD_DIR . $idPhoto['photo'])) {
+                unlink(self::UPLOAD_DIR . $idPhoto['photo']);
             }
 
             header('Location:/admin/membres');
