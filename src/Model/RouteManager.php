c5341885c8d018f98f6781a0a3e19e410cd60dd5
toomanyMG <tchiki.tshiki@gmail.com>
2022-11-06 18:27:40 +0100

    Added a method to insert into the db

--- src/Model/RouteManager.php
+++ src/Model/RouteManager.php
@@ -7,4 +7,43 @@ use PDO;
 class RouteManager extends AbstractManager
 {
     public const TABLE = 'route';
+
+
+    public function insert(array $route): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (
+            `date`,
+            `time`,
+            `start`,
+            `finish`,
+            `ravito`,
+            `distance`,
+            `difficulty`,
+            `gpx`,
+            `description`) 
+            VALUES (
+                :date,
+                :time,
+                :start,
+                :finish,
+                :ravito,
+                :distance,
+                :difficulty,
+                :gpx,
+                :description
+                )");
+
+        $statement->bindValue('date', $route['date'], PDO::PARAM_STR);
+        $statement->bindValue('time', $route['time'], PDO::PARAM_STR);
+        $statement->bindValue('start', $route['start'], PDO::PARAM_STR);
+        $statement->bindValue('finish', $route['finish'], PDO::PARAM_STR);
+        $statement->bindValue('ravito', $route['ravito'], PDO::PARAM_STR);
+        $statement->bindValue('distance', $route['distance'], PDO::PARAM_INT);
+        $statement->bindValue('difficulty', $route['difficulty'], PDO::PARAM_INT);
+        $statement->bindValue('gpx', $route['gpx'], PDO::PARAM_STR);
+        $statement->bindValue('description', $route['description'], PDO::PARAM_STR);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
 }
