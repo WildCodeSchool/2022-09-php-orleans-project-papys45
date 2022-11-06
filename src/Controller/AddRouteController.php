21a45d9d76e0ea0c14a51b4c8aad66fc548d80ea
toomanyMG <tchiki.tshiki@gmail.com>
2022-11-06 17:22:05 +0100

    Changed the method index

--- src/Controller/AddRouteController.php
+++ src/Controller/AddRouteController.php
@@ -8,9 +8,6 @@ class AddRouteController extends AbstractController
 {
     public function index(): string
     {
-        $adminMembersManager = new RouteManager();
-        $members = $adminMembersManager->selectAll();
-
         return $this->twig->render('Admin/AddRouteForm.html.twig');
     }
 }
