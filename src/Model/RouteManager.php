9e1650f15b6239d2461e815b97fff1ef4f364b0c
teddy.andria <teddy.andriamam@gmail.com>
2022-11-07 15:56:25 +0100

    add update method on RouteManager

--- src/Model/RouteManager.php
+++ src/Model/RouteManager.php
@@ -7,4 +7,21 @@ use PDO;
 class RouteManager extends AbstractManager
 {
     public const TABLE = 'route';
+
+    public function update(array $route): bool
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `date` =:date, 
+        `time`=: time,
+        `start`=: start,
+        `finish`=: finish,
+        `distance`=: distance
+        WHERE id=:id");
+        $statement->bindValue('date', $route['date'], PDO::PARAM_STR);
+        $statement->bindValue('time', $route['time'], PDO::PARAM_STR);
+        $statement->bindValue('start', $route['start'], PDO::PARAM_STR);
+        $statement->bindValue('finish', $route['finish'], PDO::PARAM_STR);
+        $statement->bindValue('distance', $route['distance'], PDO::PARAM_INT);
+
+        return $statement->execute();
+    }
 }
