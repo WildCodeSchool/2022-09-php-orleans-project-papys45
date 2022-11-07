3172dbf0ab7d21185613dc277072d8d5e21f364a
teddy.andria <teddy.andriamam@gmail.com>
2022-11-07 22:52:29 +0100

    add modal on the view page and delete function

--- src/Controller/AdminRouteController.php
+++ src/Controller/AdminRouteController.php
@@ -8,9 +8,21 @@ class AdminRouteController extends AbstractController
 {
     public function index(): string
     {
-        $routemanager = new RouteManager();
-        $route = $routemanager->selectAll('date', 'DESC');
+        $routeManager = new RouteManager();
+        $route = $routeManager->selectAll('date', 'DESC');
 
         return $this->twig->render('AdminRoute/adminRoute.html.twig', ['route' => $route]);
+    }
+
+    public function delete(): string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = trim($_POST['id']);
+            $routeManager = new RouteManager();
+            $routeManager->delete((int)$id);
+
+            header('Location:/admin/deleteRoute');
+        }
+        return '';
     }
 }
