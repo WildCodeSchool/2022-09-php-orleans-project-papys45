2177187cb7f4beeae198ab37093e7d7dd06ecf4b
Severine Helene Vilaine <severine.vilaine@gmail.com>
2022-11-08 16:43:05 +0100

    Started working on the Create part

--- src/Controller/AdminActuController.php
+++ src/Controller/AdminActuController.php
@@ -14,14 +14,4 @@ class AdminActuController extends AbstractController
 
         return $this->twig->render('Admin/admin_actuality.html.twig', ['actualities' => $actualities]);
     }
-
-    public function add(string $actuality = ''): void
-    {
-        $actualities = [];
-        $errors = [];
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $actualities = array_map('trim', $_POST);
-        }
-    }
 }
--- src/Model/ActualityManager.php
+++ src/Model/ActualityManager.php
@@ -8,9 +8,11 @@ class ActualityManager extends AbstractManager
 {
     public const TABLE = 'actuality';
 
-    public function update (array $actuality): bool
+    public function update(array $actuality): bool
     {
-        $statement = $this->pdo->prepare("UPDATE" . self::TABLE . "SET `title`=:title, `content`=:content WHERE id=:id");
+        $statement = $this->pdo->prepare("UPDATE" . self::TABLE .
+        "SET `title`=:title, `content`=:content
+        WHERE id=:id");
 
         $statement->bindValue('id', $actuality['id'], PDO::PARAM_INT);
         $statement->bindValue('title', $actuality['title'], PDO::PARAM_STR);
