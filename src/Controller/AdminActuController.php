0def4a4632f2b3b2c3085b3ce8d6bbf77f8ccf89
Severine Helene Vilaine <severine.vilaine@gmail.com>
2022-11-08 16:42:12 +0100

    Started working on the Create part

--- src/Controller/AdminActuController.php
+++ src/Controller/AdminActuController.php
@@ -14,4 +14,14 @@ class AdminActuController extends AbstractController
 
         return $this->twig->render('Admin/admin_actuality.html.twig', ['actualities' => $actualities]);
     }
+
+    public function add(string $actuality = ''): void
+    {
+        $actualities = [];
+        $errors = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $actualities = array_map('trim', $_POST);
+        }
+    }
 }
