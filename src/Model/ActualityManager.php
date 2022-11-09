0991a7033c600dd6c88409acfe4c108d5053b2db
Severine Helene Vilaine <severine.vilaine@gmail.com>
2022-11-09 16:02:37 +0100

    Added a fex things and fixed some mistakes

--- src/Model/ActualityManager.php
+++ src/Model/ActualityManager.php
@@ -8,16 +8,28 @@ class ActualityManager extends AbstractManager
 {
     public const TABLE = 'actuality';
 
-    public function update(array $actuality): bool
+    public function add(array $actuality): int
     {
-        $statement = $this->pdo->prepare("UPDATE" . self::TABLE .
-        "SET `title`=:title, `content`=:content
-        WHERE id=:id");
-
+        $statement = $this->pdo->prepare(
+            "INSERT INTO " . self::TABLE .
+                "(`title`,`content`)
+            VALUES
+            (:title, :content,)"
+        );
         $statement->bindValue('id', $actuality['id'], PDO::PARAM_INT);
         $statement->bindValue('title', $actuality['title'], PDO::PARAM_STR);
         $statement->bindValue('content', $actuality['content'], PDO::PARAM_STR);
+        $statement->execute();
 
-        return $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
+
+    public function selectOneById(int $id): array|false
+    {
+        $statement = $this->pdo->prepare("SELECT `id` FROM " . self::TABLE . " WHERE id=:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetch();
     }
 }
