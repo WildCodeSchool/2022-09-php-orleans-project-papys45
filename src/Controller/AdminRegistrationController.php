ece001fe3cf64131858f5ed94054c2877de37c02
teddyandria <teddy.andriamam@gmail.com>
2022-11-16 11:27:27 +0100

    create controller and manager for add registration

--- src/Controller/AdminRegistrationController.php
+++ src/Controller/AdminRegistrationController.php
@@ -16,4 +16,18 @@ class AdminRegistrationController extends AbstractController
             ['registrations' => $registrations]
         );
     }
+
+    public function add(int $id): string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $registration = array_map('trim', $_POST);
+
+            $registrationManager = new RegistrationManager();
+            $registrationManager->insert($registration);
+
+            header('Location:/admin/inscription/ajouter?id=' . $id);
+            return '';
+        }
+        return $this->twig->render('Admin/addRegistration.html.twig');
+    }
 }
